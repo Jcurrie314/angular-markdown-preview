9ba0b7561365e7ad8bb72c68ba8ec8a3f4f46b9f
Jonathan Currie <currie314@gmail.com>
2015-07-13 11:26:27 -0700

    fmio specific changes to the template

--- markdown_preview.js
+++ markdown_preview.js
@@ -26,7 +26,7 @@
 
   .directive('iiMdPreview', function() {
     return {
-      template: "<div><textarea class='MdPreview-markdown' name='{{textareaName}}' ng-model='markdown' ng-change='md2Html()'></textarea><div class='MdPreview-html' ng-bind-html='htmlSafe' /></div>",
+      template: "<div layout><textarea class='MdPreview-markdown' name='{{textareaName}}' ng-model='body' ng-change='md2Html()' flex='50' style='order:1 !important;'></textarea><div class='MdPreview-html' ng-bind-html='htmlSafe' flex='50' style='order:2 !important;'/></div>",
       restrict: 'C',
       replace: true,
       controller: 'Ctrl',
